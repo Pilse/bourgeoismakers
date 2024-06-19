d4cd56a18d16b39f4c865515601ddbf860c74a15
pilse <gogosky1175@gmail.com>
2024-06-19 23:07:16 +0900

    fix: fix env

--- src/app/auth/google/route.ts
+++ src/app/auth/google/route.ts
@@ -1,6 +1,17 @@
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  console.log(request);
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  const loginRes = await fetch(`${process.env.API_URL}/api/v1/auth/google`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ code }),
+  });
+  console.log("loginRes", loginRes);
+
   return Response.redirect(`${process.env.APP_URL}/main`);
 }
